172c0bf1988d3ca60f074e7131ff5378f980a71d
allan <aj6738272@gmail.com>
2025-12-05 18:55:46 +0300

    javascript-scope-lab  jestTest

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Write your solution in this file!
+// Write your solution in this file!// ---------------------------------------
+// STEP 1: Variable Data (Global Scope)
+// ---------------------------------------
+
+let burgers = ["Hamburger", "Cheeseburger"];
+let featuredDrink = "Strawberry Milkshake";
+
+
+// ---------------------------------------
+// STEP 2: Function + Function Scope (var)
+// ---------------------------------------
+
+function addBurger() {
+  var newBurger = "Flatburger";   // function-scoped
+  burgers.push(newBurger);        // add to global array
+}
+
+
+// ---------------------------------------
+// STEP 3: Block Scope + If Statement
+// ---------------------------------------
+
+if (true) { // always true
+
+  let anotherNewBurger = "Maple Bacon Burger";  // block-scoped
+  burgers.push(anotherNewBurger);               // add to array
+
+  function changeFeaturedDrink() {
+    featuredDrink = "The JavaShake";            // update global variable
+  }
+}
